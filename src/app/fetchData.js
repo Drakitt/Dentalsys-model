ed420714546cde560344b9f3393c783bd7b191b2
Drakitt <rakiterrazas@gmail.com>
2025-08-25 15:37:32 -0400

    test cors

--- src/app/fetchData.js
+++ src/app/fetchData.js
@@ -2,14 +2,13 @@ import axios from 'axios';
 
 const fetchData = async (id) => {
   try {
-      axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-
-    const response = await axios.get({
-    method: 'get',
-    url: `https://dentalsys-services.onrender.com/api/v1/odontograma/${id}`,
-    withCredentials: false
-  }
-      );
+  //   const response = await axios.get({
+  //   method: 'get',
+  //   url: `https://dentalsys-services.onrender.com/api/v1/odontograma/${id}`,
+  //   withCredentials: false
+  // }
+  //     );
+    const response = await axios.get(`https://dentalsys-services.onrender.com/api/v1/odontograma/${id}`);
     return response; 
   } catch (error) {
     throw new Error('Error fetching data'); 
