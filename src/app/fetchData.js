ef85c6f4e42642a1e3ed67b20a65bfb1ab9c5995
Drakitt <rakiterrazas@gmail.com>
2025-08-25 13:40:10 -0400

    fixed

--- src/app/fetchData.js
+++ src/app/fetchData.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 const fetchData = async (id) => {
   try {
-    const response = await axios.get(`https://dentalsys-services.onrender.com/api/v1/odontograma/${id}`,{headers: {
-                'Access-Control-Allow-Origin': '*',
-                'origin':'x-requested-with',
-                'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
-                'Content-Type': 'application/json',
-            }});
+      axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+    const response = await axios.get({
+    method: 'get',
+    url: `https://dentalsys-services.onrender.com/api/v1/odontograma/${id}`,
+    withCredentials: false
+  }
+      );
     return response; 
   } catch (error) {
     throw new Error('Error fetching data'); 
