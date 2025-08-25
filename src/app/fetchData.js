cf647816288daefe376bec55573e1947b6335b8f
Drakitt <rakiterrazas@gmail.com>
2025-08-25 13:09:24 -0400

    added headers

--- src/app/fetchData.js
+++ src/app/fetchData.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 const fetchData = async (id) => {
   try {
-    const response = await axios.get(`https://dentalsys-services.onrender.com/api/v1/odontograma/${id}`);
+    const response = await axios.get(`https://dentalsys-services.onrender.com/api/v1/odontograma/${id}`,{headers: {
+                'Access-Control-Allow-Origin': '*',
+                'origin':'x-requested-with',
+                'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
+                'Content-Type': 'application/json',
+            }});
     return response; 
   } catch (error) {
     throw new Error('Error fetching data'); 
--- src/app/getAll.js
+++ src/app/getAll.js
@@ -3,7 +3,12 @@ import { environment } from './environments/environments';
 
 const create = async (newData) => {
   try {
-    const response = await axios.post(environment.endpoint+`/api/v1/tratamientos/forOdonto`,newData);
+    const response = await axios.post(environment.endpoint+`/api/v1/tratamientos/forOdonto`,newData,{headers: {
+                'Access-Control-Allow-Origin': '*',
+                'origin':'x-requested-with',
+                'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
+                'Content-Type': 'application/json',
+            }});
     return response.data.data.rows;
   } catch (error) {
     throw error;
